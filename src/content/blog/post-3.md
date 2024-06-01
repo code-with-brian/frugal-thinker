68690638d9dfe138dc5e6a4b5128b6e13d18c27a
Brian Palmer <brian.palmer@live.com>
2024-06-01 05:41:24 -0400

    Fixed alignment for welcome page

--- src/content/blog/post-3.md
+++ src/content/blog/post-3.md
@@ -3,9 +3,6 @@ layout: ../../layouts/BlogPost.astro
 title: 10 Creative Ways to Save Money on Groceries Without Sacrificing Quality
 author: Frugal Thinker
 description: "This post will show up on its own!"
-image:
-    url: "https://docs.astro.build/default-og-image.png"
-    alt: "The word astro against an illustration of planets and stars."
 pubDate: 2022-08-08
 tags: ["astro", "successes"]
 ---
---
+++ src/content/config.ts
@@ -0,0 +1,28 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const blogCollection = defineCollection({
+  type: 'content', // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    pubDate: z.date(),
+  }),
+});
+
+const goal = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        tags: z.array(z.string()),
+        image: z.string().optional(),
+      }),
+  });
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'blog': blogCollection,
+  'goals': goal,
+};
